8f546426b6d68f99080908c715824c55133095cb
Adrien Wilmet <adrienwilmet@gmail.com>
2021-09-29 10:27:28 +0200

    Test multiple loginUser

---
+++ tests/Controller/IndexControllerTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+/**
+ * @internal
+ */
+class IndexControllerTest extends WebTestCase
+{
+    public function testIndex()
+    {
+        $client = static::createClient();
+        $userRepository = static::getContainer()->get(UserRepository::class);
+        $em = static::getContainer()->get('doctrine')->getManager();
+
+        $user1 = new User();
+        $user1->setFirstname('Toto');
+        $user1->setLastname('XX');
+        $user1->setEmail('toto@toto.com');
+        $user1->setPassword('...');
+
+        $user2 = new User();
+        $user2->setFirstname('Tata');
+        $user2->setLastname('XX');
+        $user2->setEmail('tata@tata.com');
+        $user2->setPassword('...');
+
+        $em->persist($user1);
+        $em->persist($user2);
+        $em->flush();
+
+        $client->loginUser($user1);
+        $crawler = $client->request('GET', '/index');
+        $this->assertResponseIsSuccessful();
+        $this->assertSame('Hello Toto!', $crawler->filter('h1')->text(null, true));
+
+        $client->loginUser($user2);
+        $crawler = $client->request('GET', '/index');
+        $this->assertResponseIsSuccessful();
+        $this->assertSame('Hello Tata!', $crawler->filter('h1')->text(null, true));
+    }
+}
